fa5e301126b2083a1376d9d60a8b7f384253fa3a
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 15:19:52 -0500

    Install json server to practice with API

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
-	const onClick = () => {
-		console.log('Click');
-	};
-
 	return (
 		<header className="header">
 			<h1>{title}</h1>
