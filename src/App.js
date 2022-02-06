a13ccdeea39fa8deefe3655f4de7adff93a18ae0
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 11:33:10 -0500

    Add feature to add task to the tracker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import './App.css';
 
 function App() {
@@ -25,6 +26,14 @@ function App() {
 		},
 	]);
 
+	// add task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+		console.log(task);
+	};
+
 	// delete task
 	const deleteTask = (id) => {
 		setTasks(tasks.filter((task) => task.id !== id));
@@ -42,6 +51,7 @@ function App() {
 	return (
 		<div className="container">
 			<Header />
+			<AddTask onAdd={addTask} />
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
