677b76b676002149b6b7680abb3313986481fb55
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 11:50:04 -0500

    Add toggle feature for / add task form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask';
 import './App.css';
 
 function App() {
+	const [showAddTask, setShowAddTask] = useState(false);
 	const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -50,8 +51,11 @@ function App() {
 
 	return (
 		<div className="container">
-			<Header />
-			<AddTask onAdd={addTask} />
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAdd={showAddTask}
+			/>
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
 	const onClick = () => {
 		console.log('Click');
 	};
@@ -10,7 +10,11 @@ const Header = ({ title }) => {
 	return (
 		<header className="header">
 			<h1>{title}</h1>
-			<Button color="green" text="Add" onClick={onClick} />
+			<Button
+				color={showAdd ? 'red' : 'green'}
+				text={showAdd ? 'Close' : 'Add'}
+				onClick={onAdd}
+			/>
 		</header>
 	);
 };
