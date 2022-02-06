bbc37f44a6f541ae876c8d2ab8853da7428855ae
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 16:12:57 -0500

    Add post/delete/put requests to add/delete/toggle-reminder

--- src/App.js
+++ src/App.js
@@ -24,24 +24,58 @@ function App() {
 		return data;
 	};
 
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`);
+		const data = await res.json();
+
+		return data;
+	};
+
 	// add task
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task };
-		setTasks([...tasks, newTask]);
-		console.log(task);
+	const addTask = async (task) => {
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(task),
+		});
+
+		const data = await res.json();
+
+		setTasks([...tasks, data]);
+		// const id = Math.floor(Math.random() * 10000) + 1;
+		// const newTask = { id, ...task };
+		// setTasks([...tasks, newTask]);
 	};
 
 	// delete task
-	const deleteTask = (id) => {
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE',
+		});
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
 	// toggle reminder
-	const toggleReminder = (id) => {
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
+		const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+		console.log(taskToToggle);
+		console.log(updatedTask);
+		console.log(JSON.stringify(updatedTask));
+
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(updatedTask),
+		});
+
+		const data = await res.json();
+
 		setTasks(
 			tasks.map((task) =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
+				task.id === id ? { ...task, reminder: data.reminder } : task
 			)
 		);
 	};
