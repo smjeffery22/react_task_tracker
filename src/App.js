60ca328ce962eb5622d5129aceffb7656c995603
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 01:58:42 -0500

    Add task component, render the tasks and implement feature to delete tasks

--- src/App.js
+++ src/App.js
@@ -25,10 +25,19 @@ function App() {
 		},
 	]);
 
+	// delete task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
+
 	return (
 		<div className="container">
 			<Header />
-			<Tasks tasks={tasks} />
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} />
+			) : (
+				'No Tasks To Show'
+			)}
 		</div>
 	);
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
 	return (
 		<div className="task">
-			<h3>{task.text}</h3>
+			<h3>
+				{task.text}
+				<FaTimes style={deleteStyle} onClick={() => onDelete(task.id)} />
+			</h3>
 			<p>{task.day}</p>
 		</div>
 	);
+};
+
+const deleteStyle = {
+	color: 'red',
+	cursor: 'pointer',
 };
 
 export default Task;
