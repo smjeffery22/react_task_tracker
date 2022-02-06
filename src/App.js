71c4c6457f73c8b7d5c3f6d63c7c3679afc71902
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-06 10:50:34 -0500

    Add toggle for reminder

--- src/App.js
+++ src/App.js
@@ -30,11 +30,20 @@ function App() {
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
+	// toggle reminder
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		);
+	};
+
 	return (
 		<div className="container">
 			<Header />
 			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} />
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
 				'No Tasks To Show'
 			)}
